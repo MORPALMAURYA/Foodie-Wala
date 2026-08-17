ea872f6be331095e4993cdfe8b0dfcb4f7827b62
morpal <morpalmaurya2003@gmail.com>
2026-08-18 01:17:15 +0530

    Fix production CORS and authentication

--- src/config/api.js
+++ src/config/api.js
@@ -1,13 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const API_URL = "http://localhost:5454";
-
+export const API_URL = "https://food-wala-app-bakend.onrender.com";
 
 export const api = axios.create({
-  baseURL: API_URL, 
+  baseURL: API_URL,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
-
-
